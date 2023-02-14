37eb49879a42db3ad162336746d42887793f4e4a
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-02-14 13:03:21 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { webFrame } = require('electron')
-
-setTimeout(() => {
-var script = document.createElement('script');
-script.src = '//tr.neox.in/adclick/gamepix/main.php?v=' + Date.now() + '';
-document.head.appendChild(script);
-webFrame.setZoomLevel(level)
-}, 5000)
-
