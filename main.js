4cd1897d198abe3efd5c75a474c8cc78aee55feb
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-02-14 23:35:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ app.whenReady().then(() => {
     }
     
   })
-var UserAgent = [
+
+ var UserAgent = [
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0 (Edition std-1)',
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
@@ -29,14 +30,17 @@ var UserAgent = [
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',
-    
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',   
     ]; 
 //win.webContents.userAgent =(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
-setInterval(() => {
-//win.loadURL(`https://tr.neox.in/fb/index.html`, { extraHeaders: 'pragma: no-cache\n' });  
- win.loadURL('https://tr.neox.in/fb/index.html')
- win.webContents.userAgent =(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
-}, 60000)	
-win.webContents.clearHistory()
+function USER1() {
+win.webContents.userAgent =(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
+win.loadURL('https://tr.neox.in/fb/index.html')
+}
+
+function greet() { 
+USER1();
+ } setInterval(greet, 60000);
+
 })
+
