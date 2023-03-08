41ac8bdc2bcf79823e562605706cd6cc60e703c9
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-08 13:19:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-const electron = require('electron')
+const { app, BrowserWindow, webContents } = require('electron')
 const url = require('url')
 const path = require('path')
 
-const { app, BrowserWindow } = electron
-let win
 
 app.whenReady().then(() => {
-  const win = new BrowserWindow({
-    width: 800,
-    webPreferences: {
-      nodeIntegration: true, // default in Electron >= 5
-      preload: path.join(__dirname, 'preload.js')
-      
-    }
-    
-  })
+const mainWindow = new BrowserWindow({
+height: 250, width: 350, webPreferences: { nodeIntegration: true, enableRemoteModule: true, preload: path.join(__dirname, 'preload.js') } })
 
- var UserAgent = [
+var UserAgent = [
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0 (Edition std-1)',
     'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0',
@@ -32,17 +23,11 @@ app.whenReady().then(() => {
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 YaBrowser/23.1.2 Yowser/2.5 Safari/537.36',   
-    ]; 
-//win.webContents.userAgent =(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
-function USER1() {
-win.webContents.userAgent =(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
-win.loadURL('https://tr.neox.in/fb/index.html?v=' + Date.now() + '')
-}
+    ];
 
-function greet() { 
-win.webContents.session.clearCache();  
-USER1();
- } setInterval(greet, 65000);
-
+ function USER1() {
+    mainWindow.webContents.setUserAgent(UserAgent[Math.floor(Math.random()*UserAgent.length)]); 
+    const options = { extraHeaders: 'pragma: no-cache\n' , httpReferrer: 'http://www.googleadservices.com/pagead/aclk?sa=loding_page' }
+    mainWindow.webContents.loadURL('https://tr.neox.in/fb/main.php?v=' + Date.now() + '', options)
+  } setInterval(USER1, 60000)
 })
-
